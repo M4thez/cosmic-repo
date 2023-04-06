3daec9829c40dcf9d49af0aed48c411ba6946c5d
M4thez <namemat@gmail.com>
2023-04-06 17:21:06 +0200

    Removed blank page

--- cosmic-app/src/main.jsx
+++ cosmic-app/src/main.jsx
@@ -9,9 +9,7 @@ import './index.scss'
 
 import Mars from './pages/Mars/components/Mars'
 import { App } from './App'
-// import { LinkHub } from './pages/LinkHub/components/LinkHub'
 import { Account } from './pages/Account/components/Account'
-import { About } from './pages/About Page/components/About'
 import { Apod } from './pages/APOD/components/Apod'
 import { Quizes } from './pages/Quizes/components/Quizes'
 import { LinkHub } from './pages/LinkHub/components/LinkHub'
@@ -25,7 +23,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Routes>
           <Route path="/" element={<App />}>
             <Route path="/mars" element={<Mars />} />
-            <Route path="/about" element={<About />} />
             <Route path="/account" element={<Account />} />
             <Route path="/apod" element={<Apod />} />
             <Route path="/solar" element={<Solar />} />
--- cosmic-app/src/pages/About Page/components/About.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/About.css";
-
-export function About() {
-  return (
-    <div>
-      <h1>Welcome to about page!</h1>
-    </div>
-  );
-}
