1ac651f8f7ac3a213741fe057c83186df4dc4e8d
M4thez <namemat@gmail.com>
2023-06-07 16:30:54 +0200

    Quizes updated, local fonts del

--- src/pages/Quizes/components/Quizes.jsx
+++ src/pages/Quizes/components/Quizes.jsx
@@ -3,10 +3,8 @@ import { useSelector } from 'react-redux'
 
 import { updateDoc, doc, getDoc } from 'firebase/firestore'
 import { db, auth } from '../../../infrastructure/firebase/firebase'
-
 import styles from '../styles/Quizes.module.scss'
-
-import quizes from './Questions.json'
+import { Link } from 'react-router-dom'
 
 export function Quizes() {
   const [currentQuestionIndex, addIndex] = useState(0)
@@ -19,7 +17,9 @@ export function Quizes() {
   const [answerCorrect, setAnswerCorrect] = useState(true)
   const [quizesFirestore, setQuizesFirestore] = useState({})
 
-  //quuestions ref
+  console.log('quizesFirestore:', quizesFirestore)
+
+  //questions ref
   const questionsRef = doc(db, 'quizes', 'questions')
 
   //getting user login state
@@ -32,7 +32,7 @@ export function Quizes() {
   }
 
   async function toggleNextQuestion() {
-    if (currentQuestionIndex < 9) {
+    if (currentQuestionIndex < quizesFirestore[chosenQuiz].length - 1) {
       addIndex(currentQuestionIndex + 1)
       setExplanationVisibility(false)
       setQuizVisible(true)
@@ -45,18 +45,18 @@ export function Quizes() {
           const currentUserRef = doc(db, 'users', auth.currentUser.uid)
           console.log('saving score', currentUserRef)
           if (chosenQuiz === 'marsQuestions') {
-            await updateDoc(currentUserRef, { marsQuiz: `${score}/10` })
+            await updateDoc(currentUserRef, { marsQuiz: `${score}/${quizesFirestore[chosenQuiz].length}` })
           } else if (chosenQuiz === 'solarQuestions') {
-            await updateDoc(currentUserRef, { solarQuiz: `${score}/10` })
+            await updateDoc(currentUserRef, { solarQuiz: `${score}/${quizesFirestore[chosenQuiz].length}` })
           } else if (chosenQuiz === 'vehicleQuestions') {
-            await updateDoc(currentUserRef, { vehicleQuiz: `${score}/10` })
+            await updateDoc(currentUserRef, { vehicleQuiz: `${score}/${quizesFirestore[chosenQuiz].length}` })
           }
         }
       } catch (error) {
         console.log('Error when upadting quiz score', error)
       }
     }
-    if (currentQuestionIndex + 1 === 10) {
+    if (currentQuestionIndex + 1 === quizesFirestore[chosenQuiz].length) {
       setExplanationVisibility(false)
       setSummaryVisibility(true)
     }
@@ -100,63 +100,69 @@ export function Quizes() {
         <div className={styles['quiz-choice']}>
           <h1>Choose the Quiz</h1>
           <div className={styles['choice-buttons']}>
-            <button className={styles['choice-button']} onClick={() => choseQuiz('marsQuestions')}>
-              Mars Quiz
+            <button onClick={() => choseQuiz('marsQuestions')}>
+              Planet Mars
             </button>
-            <button className={styles['choice-button']} onClick={() => choseQuiz('solarQuestions')}>
-              Solar System Quiz
+            <button onClick={() => choseQuiz('solarQuestions')}>
+              Solar System
             </button>
-            <button className={styles['choice-button']} onClick={() => choseQuiz('vehicleQuestions')}>
-              NASA Missions Quiz
+            <button onClick={() => choseQuiz('vehicleQuestions')}>
+              NASA Missions
             </button>
           </div>
         </div>
       )}
-      {(quizVisible || explanationVisible) && <h1>Question {currentQuestionIndex + 1}/10</h1>}
-      {quizVisible && (
-        <div className={styles['quiz-box']}>
-          {currentQuestionIndex < 10 && (
-            <div className={styles['question-box']}>
-              <h3>{quizesFirestore[chosenQuiz][currentQuestionIndex].question}</h3>
-              <div className={styles['answer-buttons']}>
-                {quizesFirestore[chosenQuiz][currentQuestionIndex].answers.map((elem) => (
-                  <button key={elem.answer} onClick={() => answerChosen(elem.isTrue)}>
-                    {elem.answer}
-                  </button>
-                ))}
-              </div>
-            </div>
-          )}
-        </div>
-      )}
 
-      {explanationVisible && (
-        <div className={styles['quiz-box']}>
-          <h4 className={answerCorrect ? styles['correct'] : styles['incorrect']}>
-            {quizes[chosenQuiz][currentQuestionIndex].explanation}
-          </h4>
-          <button
-            className={styles['next-question-btn']}
-            onClick={() => {
-              toggleNextQuestion()
-            }}
-          >
-            {currentQuestionIndex + 1 === 10 ? 'Show score' : 'Next question'}
-          </button>{' '}
-        </div>
-      )}
+      <div className={styles['quiz-box']}>
+        {(quizVisible || explanationVisible) && (
+          <h2>
+            Question {currentQuestionIndex + 1}/{quizesFirestore[chosenQuiz].length}
+          </h2>
+        )}
+        {quizVisible && currentQuestionIndex < quizesFirestore[chosenQuiz].length && (
+          <div className={styles['quiz-content-box']}>
+            <p>{quizesFirestore[chosenQuiz][currentQuestionIndex].question}</p>
+            <div className={styles['answer-buttons']}>
+              {quizesFirestore[chosenQuiz][currentQuestionIndex].answers.map((elem) => (
+                <button key={elem.answer} onClick={() => answerChosen(elem.isTrue)}>
+                  {elem.answer}
+                </button>
+              ))}
+            </div>
+          </div>
+        )}
+
+        {explanationVisible && (
+          <div className={styles['quiz-content-box']}>
+            <p className={answerCorrect ? styles['correct'] : styles['incorrect']}>
+              {quizesFirestore[chosenQuiz][currentQuestionIndex].explanation}
+            </p>
+            <button
+              onClick={() => {
+                toggleNextQuestion()
+              }}
+            >
+              {currentQuestionIndex + 1 === quizesFirestore[chosenQuiz].length ? 'Show score' : 'Next question'}
+            </button>{' '}
+          </div>
+        )}
+      </div>
 
       {summaryVisible && (
-        <div className={styles['question-box']}>
-          <h3>Congrats you answered correctly {score} out of 10 questions!</h3>
-          <button
-            className={styles['next-question-btn']}
-            onClick={() => {
-              retryQuiz()
-            }}
-          >
-            Retry
-          </button>
+        <div className={styles['summary-box']}>
+          <p>
+            Congrats you answered correctly {score} out of {quizesFirestore[chosenQuiz].length} questions!
+          </p>
+          <div className={styles['answer-buttons']}>
+            <button
+              onClick={() => {
+                retryQuiz()
+              }}
+            >
+              Retry
+            </button>
+            <Link to="/" className={styles['link-button']}>Go Home</Link>
+          </div>
         </div>
       )}
     </div>
