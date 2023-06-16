76d61761b0d49a48be010f297d62e9716ec3b4d8
M4thez <namemat@gmail.com>
2023-06-16 16:47:14 +0200

    Update loading screen styling #2

--- src/infrastructure/loader/LoaderCustom.jsx
+++ src/infrastructure/loader/LoaderCustom.jsx
@@ -4,8 +4,8 @@ export default function LoaderCustom() {
   return (
     <Loader
     containerStyles={{'background': '#040404'}}
-    innerStyles={{'width': '33vw','height': '16px'}}
-    barStyles={{'width': '33vw', 'height': '16px'}}
+    innerStyles={{'width': '38vw','height': '16px'}}
+    barStyles={{'width': '38vw', 'height': '16px'}}
     dataStyles={{'fontSize': '20px'}}
     dataInterpolation={(p) => `Loading ${p.toFixed(0)}%`}
     />
